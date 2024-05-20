04dc42deaa2c1aaefdac23a96a556f29b58f451a
dingyang.chen <chen0z7dy@gmail.com>
2024-05-20 15:01:54 +0200

    1st commit

---
+++ src/main/java/com/example/webclientspringboot/WebClientSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.webclientspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientSpringbootApplication.class, args);
+    }
+
+}
